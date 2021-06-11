90e72f27b9b475125c4f1f406670edb23c2c7747
Michael Kret <dirtysculptor@gmail.com>
2021-06-11 07:45:34 +0300

    learned how to extend tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,20 @@
 module.exports = {
   purge: {
     enabled: false,
-    content: ['./*.html'],
+    content: ["./*.html"],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        insure: "hsl(256, 26%, 20%)",
+      },
+    },
   },
+  extend: {},
+
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
