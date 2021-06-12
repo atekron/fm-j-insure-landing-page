34f37cf067931cc9788d5bc2401a60e7451ef46d
Michael Kret <dirtysculptor@gmail.com>
2021-06-12 07:33:19 +0300

    working on desktop layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,13 @@
+// --------------------------------------------------------------------
+// buid tailwind:
+// npx tailwindcss-cli@latest build ./src/tailwind.css -o ./styles.css
+// --------------------------------------------------------------------
+// when buiding tailwind for production:
+// module.exports = {
+//   purge: {
+//     enabled: true, <----- set true to remove unused styles ---------
+// --------------------------------------------------------------------
+
 module.exports = {
   purge: {
     enabled: false,
@@ -7,7 +17,16 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        insure: "hsl(256, 26%, 20%)",
+        // keep original  colors and extend with your own
+        "insure-violet": "hsl(256, 26%, 20%)",
+        "insure-blue": "hsl(216, 30%, 68%)",
+        "insure-dark-violet": "hsl(270, 9%, 17%)",
+        "insure-gray-violet": "hsl(273, 4%, 51%)",
+        "insure-grey-light": "hsl(0, 0%, 98%)",
+      },
+      fontFamily: {
+        "dm-serif": ['"DM Serif Display"', "serif"],
+        karla: ['"Karla"', "sans-serif"],
       },
     },
   },
