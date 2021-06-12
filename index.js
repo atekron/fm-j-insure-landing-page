c5bff115f7dcf564df1d38a0f3a7a1e36fb3d86e
Michael Kret <dirtysculptor@gmail.com>
2021-06-12 22:49:18 +0300

    challenge done

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-
+document.getElementById("mobile-menu").addEventListener("click", () => {
+  document.getElementById("mobile-hamburger").classList.toggle("hidden");
+  document.getElementById("mobile-close").classList.toggle("hidden");
+  document.getElementById("main-nav").classList.toggle("hidden");
+});
